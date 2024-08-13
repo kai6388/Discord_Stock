0f9e30ca0a8c157bde5533faa1647e8a4a2623d5
kai6388 <alsxhfdl4314@gmail.com>
2024-08-13 12:16:36 +0900

    2

--- Discord_Stock.py
+++ Discord_Stock.py
@@ -17,34 +17,34 @@ if TOKEN is None:
 # 조회할 주식 티커 리스트
 STOCK_TICKERS = ['TQQQ', 'NVDA', 'NVDL']  # 원하는 주식 티커를 추가하세요
 
+# 인텐트 설정
 intents = discord.Intents.default()
-client = discord.Client(intents=intents)
+intents.message_content = True  # 메시지 내용 인텐트 활성화# 메시지 내용에 접근할 수 있도록 설정
+
+# Bot 객체 생성
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 # 스케줄러 초기화
 scheduler = AsyncIOScheduler()
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'Logged in as {client.user}')
+    print(f'Logged in as {bot.user}')
     
-    # 스케줄러에 작업 추가: 매일 오전 5시 30분에 stock_price_notification 함수를 실행
+    # 매일 오전 5시 30분에 stock_price_notification 함수를 실행하도록 스케줄러 설정
     scheduler.add_job(stock_price_notification, 'cron', hour=5, minute=30)
     scheduler.start()
 
-@client.event
-async def on_message(message):
-    # 봇이 보낸 메시지는 무시
-    if message.author == client.user:
-        return
-    
-    # 특정 명령어 입력 시 주식 가격 알림 실행
-    if message.content.startswith('!종가'):
-        await stock_price_notification()
+@bot.command(name='종가')
+async def stock_price(ctx):
+    await stock_price_notification(ctx.channel)
 
-async def stock_price_notification():
+async def stock_price_notification(channel=None):
     try:
-        # 메시지를 보낼 채널 객체 가져오기
-        channel = client.get_channel(CHANNEL_ID)
+        # 채널이 지정되지 않은 경우 기본 채널 사용
+        if channel is None:
+            channel = bot.get_channel(CHANNEL_ID)
+        
         if channel is not None:
             # 각 주식 티커에 대해 종가를 가져와서 메시지로 전송
             for ticker in STOCK_TICKERS:
@@ -58,5 +58,5 @@ async def stock_price_notification():
         if channel is not None:
             await channel.send(f"주식 정보를 가져오는데 실패했습니다: {e}")
 
-
-client.run(TOKEN)
+# 봇 실행
+bot.run(TOKEN)
